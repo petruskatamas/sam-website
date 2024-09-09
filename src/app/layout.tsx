9f63d666773fd1f79b071fec916b99bdbfba27de
Petruska Tomi <dev.tamaspetruska@gmail.com>
2024-09-09 11:18:34 +0200

    build: facbook-pixel script and package update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -5,6 +7,7 @@ import { NavBar } from '@/components/custom/generic-components/nav/NavBar'
 import { Footer } from '@/components/custom/generic-components/Footer'
 import { Toaster } from '@/components/ui/toaster'
 import CookieBanner from '@/components/custom/generic-components/CookieBanner'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,6 +30,28 @@ export default function RootLayout({
         <Toaster />
         <CookieBanner />
       </body>
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '8569448136421117');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=8569448136421117&ev=PageView&noscript=1"
+        />
+      </noscript>
     </html>
   )
 }
